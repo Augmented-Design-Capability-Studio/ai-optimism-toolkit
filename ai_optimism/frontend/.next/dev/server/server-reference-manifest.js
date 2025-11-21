579d55f230e5646f26c8196776e9c5a93e885684
Howie Wang <howiewang@cmu.edu>
2025-11-20 21:42:31 -0500

    Add emotion cache and dependency; stop tracking Next build artifacts

--- ai_optimism/frontend/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"6JfnnRIyimH/SHa+byJYTSBKqkJgvfQ/5ir2rg3jjKM=\"\n}"
--- ai_optimism/frontend/.next/dev/static/chunks/app_layout_tsx_78cdd4a3._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_7f575ade._.js",
-  "static/chunks/_5003eeb0._.js",
-  "static/chunks/_8f20e414._.css"
-],
-    source: "dynamic"
-});
